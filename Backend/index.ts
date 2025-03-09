6e560d827f8543a09009c1401cc83d4fae8c5304
Hoang Lam <hoangchilam@pm.me>
2025-03-09 19:38:17 +0700

    chore: update be

--- Backend/index.ts
+++ Backend/index.ts
@@ -2,13 +2,19 @@ import { PrismaClient } from '@prisma/client';
 import { enhance, type auth } from '@zenstackhq/runtime';
 import { createHonoHandler } from '@zenstackhq/server/hono';
 import { type Context, Hono } from 'hono';
+import { RestApiHandler } from '@zenstackhq/server/api';
+
+
 
 const prisma = new PrismaClient();
 const app = new Hono();
 
+const handler = RestApiHandler({ endpoint: 'http://localhost:3000/api/model' });
+
 app.use(
     '/api/model/*',
     createHonoHandler({
+        handler,
         getPrisma: (ctx) => {
             return enhance(prisma, { user: getCurrentUser(ctx) });
         },
@@ -19,7 +25,7 @@ app.use(
 function getCurrentUser(ctx: Context): auth.User {
     // the implementation depends on your authentication mechanism
 
-    return { id: 1 }
+    return { id: BigInt(1) }
 }
 
 export default {
